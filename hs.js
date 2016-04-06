4855060be47b3c612a571b13a207b6ee277a997e
Uriel Sarrazin <uriel@urielsarrazin.com>
2016-04-06 15:07:45 +0200

    Minor changes

--- hs.js
+++ hs.js
@@ -1,11 +1,10 @@
 item({desc: 'partial-pressure-calculator'}, function() {
 
-    github = 'https://github.com/urielsarrazin/partial-pressure-calculator'
     linux = exec('uname').indexOf('Linux') > -1
     browser = linux ? 'sensible-browser' : 'open'
 
     item({key: 'b', desc: 'build', cmd: 'mvn clean install'})
     item({key: 'r', desc: 'run', cmd: './target/appassembler/bin/partial-pressure-calculator'})
-    item({key: 'd', desc: 'documentation', cmd: browser + ' ' + github})
-    item({key: 'f', desc: 'file bug report', cmd: browser + ' ' + github + '/issues/new'})
+    item({key: 'd', desc: 'documentation', cmd: browser + ' ' + 'https://urielsarrazin.github.io/partial-pressure-calculator/'})
+    item({key: 'f', desc: 'file bug report', cmd: browser + ' ' + 'https://github.com/urielsarrazin/partial-pressure-calculator/issues/new'})
 })
